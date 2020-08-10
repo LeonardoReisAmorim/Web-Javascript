938195b6a6ee1b6a1afab727cc07998505bdb861
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2020-08-10 18:41:07 -0300

    up reparos

--- notas alunos e media js/js/script.js
+++ notas alunos e media js/js/script.js
@@ -3,18 +3,28 @@ document.querySelector('#btnexecutar').addEventListener('click', function(){
     let notas = [];
     let soma = 0;
     let media = 0;
+    let erro = 0;
     notas.push(parseFloat(document.getElementById('txtaluno1').value));
     notas.push(parseFloat(document.getElementById('txtaluno2').value));
     notas.push(parseFloat(document.getElementById('txtaluno3').value));
     for(let i in notas){
-        soma += notas[i];
+        if(notas[i]>10){
+            erro = 1;
+        }else{
+            soma += notas[i];
+        }
     }
-    media = soma/notas.length;
-    document.querySelector('#a1').innerHTML = notas[0];
-    document.querySelector('#a2').innerHTML = notas[1];
-    document.querySelector('#a3').innerHTML = notas[2];
-    let situ = situaluno(media);
-    document.querySelector('#situacao').innerHTML = situ + " sua media e: "+media.toFixed(1);
+    if(erro===1){
+        alert("dados invalidos, insira uma nota menor que 10");
+    }else{
+        media = soma/notas.length;
+        document.querySelector('#a1').innerHTML = notas[0];
+        document.querySelector('#a2').innerHTML = notas[1];
+        document.querySelector('#a3').innerHTML = notas[2];
+        let situ = situaluno(media);
+        document.querySelector('#situacao').innerHTML = situ + " sua media e: "+media.toFixed(1);
+    }
+    
 })
 
 function situaluno(x){
